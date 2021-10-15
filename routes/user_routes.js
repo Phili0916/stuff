66087f9b11683ee4415a150e6295d823f3669f23
Phil Davis <phil@factoryz.fr>
2021-10-15 12:36:42 +0200

    updated getUser

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -2,9 +2,36 @@ const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../Controllers/user_controllers')
+const {body, validationResult} = require('express-validator')
+const auth = require("../middleware/auth");
+const stuffCtrl = require("../Controllers/stuff_controller");
+//const stuffCtrl = require("../Controllers/stuff_controller");
 
-router.post( '/signup', userCtrl.signup)
-router.post('/login', userCtrl.login)
+router.post( '/signup',
+    userCtrl.signup)
+
+
+router.post('/login',
+    body('email').notEmpty(),
+    body('password').notEmpty(),
+    (req, res, next) => {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).send({errors: errors.array()});
+      }
+      next()
+    },
+    userCtrl.login)
+
+router.get('/getUser/',
+    auth,
+    async function (req, res, next) {
+      let params = req.query
+      await userCtrl.getUser(req, res, params)
+    }
+)
+
 router.delete('/delete', userCtrl.delete)
 
 
